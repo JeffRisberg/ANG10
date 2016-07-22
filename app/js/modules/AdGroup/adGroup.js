e0146cd23a2fc9e9aba0723869f3230177b54330
JeffRisberg <jrisberg@aol.com>
2016-07-21 18:11:46 -0700

    split the sccs files into 4 parts

--- app/js/modules/AdGroup/adGroup.js
+++ app/js/modules/AdGroup/adGroup.js
@@ -1,6 +1,8 @@
 import AdGroupController from "./AdGroupController";
 import AdGroupService from "../..//services/AdGroupService";
 
+require("../../../styles/ang10adGroups.scss");
+
 var adGroupModule = angular.module("AdGroup", []);
 
 adGroupModule.controller("AdGroupController", AdGroupController);
--- app/js/modules/Keyword/keyword.js
+++ app/js/modules/Keyword/keyword.js
@@ -1,6 +1,8 @@
 import KeywordController from "./KeywordController";
 import KeywordService from "../../services/KeywordService";
 
+require("../../../styles/ang10keywords.scss");
+
 var keywordModule = angular.module("Keyword", []);
 
 keywordModule.controller("KeywordController", KeywordController);
