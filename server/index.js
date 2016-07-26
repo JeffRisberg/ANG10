b6bef9474455e18f0744005a9833ff699c31605b
JeffRisberg <jrisberg@aol.com>
2016-07-25 19:42:31 -0700

    took out use of jQuery

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ var app = express();
 
 var mocks = globSync('./mocks/**/*.js', {cwd: __dirname}).map(require);
 
+const PATH_FONTS = path.resolve(__dirname, '../app/fonts');
 const PATH_STYLES = path.resolve(__dirname, '../app/styles');
 const PATH_IMAGES = path.resolve(__dirname, '../app/images');
 const PATH_JS = path.resolve(__dirname, '../app/js');
-
 const PATH_DIST = path.resolve(__dirname, '../dist');
 
+app.use('/fonts', express.static(PATH_FONTS));
 app.use('/styles', express.static(PATH_STYLES));
 app.use('/images', express.static(PATH_IMAGES));
 app.use('/js', express.static(PATH_JS));
